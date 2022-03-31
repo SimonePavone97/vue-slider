f4fd71d547f04084f773c47d7268d63b466c986a
SimonePavone <simonebest56@gmail.com>
2022-03-31 17:54:33 +0200

    Problema img center

---
+++ main.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+        el: '#root',
+        data: {
+            immagini: [
+                'https://source.unsplash.com/random/?shoes',
+                'https://source.unsplash.com/random/?fruits',
+                'https://source.unsplash.com/random/?city'
+            ]
+        }
+    }
+)
+
+
+/*var app = new Vue(
+  {
+    el: '#root',
+    data: {
+      immagini: [
+        'https://source.unsplash.com/random/?shoes',
+        'https://source.unsplash.com/random/?fruits',
+        'https://source.unsplash.com/random/?city'
+      ],*/
